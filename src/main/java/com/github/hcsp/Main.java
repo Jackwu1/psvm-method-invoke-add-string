49107c65243bd69f67ba98cbdd3aa5f270586edc
wuzhenquan <782754354@qq.com>
2026-09-22 16:48:10 +0800

    feat: 编写一个带String参数的方法

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -8,4 +8,8 @@ public class Main {
     // Create an add(String a, String b) method to concatenate two strings
     // 在这里编写一个add(String a, String b)方法进行字符串连接
 
+    private static String add(String name1, String name2) {
+        return name1 + name2;
+    }
+
 }
